7523a5bb05b4e249d8ef9f757b02cbfb0bee88ff
0xf61 <16033911+0xF61@users.noreply.github.com>
2024-07-17 18:46:44 +0000

    fix(lsp)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
-
-keymap.set("n", "x", '"_x')
-
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
--- Move window
-keymap.set("n", "<C-h>", "<C-w>h")
-keymap.set("n", "<C-k>", "<C-w>k")
-keymap.set("n", "<C-l>", "<C-w>l")
-keymap.set("n", "<C-j>", "<C-w>j")
+-- Add any additional keymaps here
